8a6445f0ae5ff3d55b89259e332a87cc3fc47ba6
Jhamie Acebuche <acebuche.jhamie@gmail.com>
2017-07-22 14:34:25 +0800

    refactor and restyling

--- todolist/assets/js/todolist.js
+++ todolist/assets/js/todolist.js
@@ -1,34 +1,21 @@
-var todos = [];
-
 $("#add-button").on('click', function(){
-	$("#todo").toggle();
+	$("input[type='text']").fadeToggle();
 });
 
-$("#todo").on('keypress', function(event){
-	if (event.which === 13) {
-		var lis = todos.length;
-		todos.push($("#todo").val());
-		
-		$("#list").append("<li id=\"todo"+lis+ "\"><button id=\"delete-button-"+lis+"\">x</button>"+$("#todo").val()+"</li>");
-		$("#todo").val("");
+$("ul").on("click", "li", function(){
+    $(this).toggleClass("done");
+});
 
-		$("#todo"+lis).on('mouseenter',function(){
-			$("#"+$(this)[0].childNodes[0].id).toggle("slide");
-		});
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
 
-		$("#todo"+lis).on('mouseleave',function(){
-			$("#"+$(this)[0].childNodes[0].id).toggle("slide");
-		});
-
-		$("#todo"+lis).on('click',function(){
-			$(this).toggleClass("done");
-		});
-		
-		$("#delete-button-"+lis).on('click',function(){
-			$(this).parent().toggle("done");
-			$(this).parent().fadeOut(1000, function(){
-				$(this).remove();
-			});
-		});
-	}
+$("input[type='text']").on('keypress', function(event){
+    if (event.which === 13) {
+        $("ul").append("<li><span><i class=\"fa fa-trash\"></i></span>"+$(this).val()+"</li>");
+        $(this).val("");
+    }
 });
